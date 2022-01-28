715d27c99b67b18a52aa29688a482aefdac98d73
EJ McVey <ej.mcvey@geopal-solutions.com>
2022-01-28 16:43:47 +0000

    Updated types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,10 @@
 export type RecipeState = {
-  name: string;
   id: string;
+  strMeal: string;
+  strArea: string;
+  strTags: string;
+  strYoutube: string;
+  strCategory: string;
+  strInstructions: string;
+  strMealThumb: string;
 };
