8c1927cdca66ae64e0da8eee4e6a5564753d54d6
EJ McVey <ej.mcvey@geopal-solutions.com>
2022-01-28 16:44:46 +0000

    Navigation edits

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css';
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
+import ViewRecipes from './components/ViewRecipes';
+import Header from './components/Header';
 
 function App() {
   return (
+
    <BrowserRouter>
+    <Header />
      <Routes>
        <Route path='/' element={<LandingPage />} />
+       <Route path='/view' element={<ViewRecipes />} />
      </Routes>
    </BrowserRouter>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
 import { Flex, Image, Text } from "@chakra-ui/react"
+import { useNavigate } from "react-router-dom"
 
 const Header = () => {
+
+  const navigate = useNavigate();
+
   return (
     <Flex justifyContent='start' m={4}>
-      <Image height='50px' src='https://i.ibb.co/tXjJRVf/logo-yummy.png' alt='website logo' />
+      <Image cursor='pointer' onClick={() => navigate('/')} height='50px' src='https://i.ibb.co/tXjJRVf/logo-yummy.png' alt='website logo' />
       {/* <Text fontSize='md'>View Recipes</Text> */}
     </Flex>
 
