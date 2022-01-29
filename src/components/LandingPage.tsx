71ddfeb26ad910e2cb3199057593fb009bc8e5f5
EJ McVey <ej.mcvey@geopal-solutions.com>
2022-01-29 15:50:56 +0000

    simplify redirect fn

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -5,11 +5,6 @@ const LandingPage = () => {
 
   const navigate = useNavigate();
 
-  const redirect = () => {
-    console.log('re');
-    navigate('/view');
-  }
-
     return (
       <>
      
@@ -18,7 +13,7 @@ const LandingPage = () => {
         <Text color='teal.300' mb={4} fontSize='5xl'>Build A Customised List With All Your Favourite Recipes</Text>
         <Text as='i' fontSize='lg'>Click the button below to view your previously saved and loved recipes!</Text>
         <Box>
-          <Button onClick={() => redirect()} mt={10} colorScheme='teal'>View Recipes</Button>
+          <Button onClick={() => navigate('/view')} mt={10} colorScheme='teal'>View Recipes</Button>
         </Box>
        </Flex>
        <Image src='https://i.ibb.co/ZKxbfTD/recipe-plate-removebg-preview.png' alt='font food image' />
